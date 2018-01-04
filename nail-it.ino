72b08fc7128ea5e9c625f9f082d3643d82e88008
Lionel R. Saposnik <slionelr@gmail.com>
2018-01-04 23:59:06 +0200

    basic working model
    knowns to calibrate the gyro on serial char input and can recive data
    from it.

--- nail-it.ino
+++ nail-it.ino
@@ -1,19 +1,20 @@
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#include "Wire.h"
-
-#include "RingBuffer.h"
-
+#include <I2Cdev.h>
+#include <Wire.h>
 #include <ArduinoLog.h>
+#include <MPU6050_6Axis_MotionApps20.h>
 
 #define DEBUG // TODO: delete this in prod
 
+#define BDU         115200
+#define WIRE_CLOCK  400000 
+
 #define INTERRUPT_PIN 2
-#define LED_INDICATOR 7
 #define SPEAKER_PIN   8
+#define LED_INDICATOR 7
+#define VIB_SENS_1    10
 
 // Led status
-int bHigh = HIGH;
+int intLed;
 
 // MPU control/status vars
 MPU6050 mpu;
@@ -23,6 +24,15 @@ uint8_t devStatus;      // return status after each device operation (0 = succes
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// Calibration
+bool isCalibrated = false;
+int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
+int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
+int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
+int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
+int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
+int16_t ax, ay, az,gx, gy, gz;
 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
@@ -49,14 +59,13 @@ void dmpDataReady() {
 
 void setup() {
   // Set monitor output BDU
-  Serial.begin(115200);
+  Serial.begin(BDU);
 
   #ifdef DEBUG
-  log_level = LOG_LEVEL_TRACE;
+  Log.begin(LOG_LEVEL_TRACE, &Serial, true);
   #else
-  log_level = LOG_LEVEL_NOTICE;
+  Log.begin(LOG_LEVEL_NOTICE, &Serial, true);
   #endif
-  Log.begin(log_level, &Serial, true);
 
   // Set led inidicator for arduino stacking
   pinMode(LED_INDICATOR, OUTPUT);
@@ -64,7 +73,7 @@ void setup() {
   
   // Start connection and set speed to the Gyro sensor
   Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Wire.setClock(WIRE_CLOCK); // 400kHz I2C clock. Comment this line if having compilation difficulties
 
   // Init the gyro
   mpu.initialize();
@@ -80,12 +89,18 @@ void setup() {
 
   // DMP init and Gyro/Accel offset *basic* reset
   devStatus = mpu.dmpInitialize();
-  mpu.setXAccelOffset(-669);
-  mpu.setYAccelOffset(-450);
-  mpu.setZAccelOffset(1963);
-  mpu.setXGyroOffset(74);
-  mpu.setYGyroOffset(-16);
-  mpu.setZGyroOffset(-38);
+//  mpu.setXAccelOffset(-669);
+//  mpu.setYAccelOffset(-450);
+//  mpu.setZAccelOffset(1963);
+//  mpu.setXGyroOffset(74);
+//  mpu.setYGyroOffset(-16);
+//  mpu.setZGyroOffset(-38);
+    mpu.setXAccelOffset(0);
+    mpu.setYAccelOffset(0);
+    mpu.setZAccelOffset(0);
+    mpu.setXGyroOffset(0);
+    mpu.setYGyroOffset(0);
+    mpu.setZGyroOffset(0);
 
   if (devStatus == 0) {
       Log.trace("Enabling DMP...\n");
@@ -115,28 +130,23 @@ void setup() {
 }
 
 void loop() {
-  // Loop led indicator change mode
-  bHigh = (bHigh == HIGH) ? LOW : HIGH;
-  digitalWrite(LED_INDICATOR, bHigh);
-  
   // if programming failed, don't try to do anything
   if (!dmpReady) {
-    return;
+      return;
   }
 
-  // wait for MPU interrupt or extra packet(s) available
-  //while (!mpuInterrupt && fifoCount < packetSize) {
-      // other program behavior stuff here
-      // .
-      // .
-      // .
-      // if you are really paranoid you can frequently test in between other
-      // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-      // while() loop to immediately process the MPU data
-      // .
-      // .
-      // .
-  //}
+  if (!isCalibrated) {
+    // TODO: Need to be changed to the finger vibration signal - wait in the loop until the signal
+    while (!Serial.available()){
+      flashLed();
+      Log.notice(F("Send any character to start calibration.\n"));
+      delay(1500);
+    }
+    calibrate();
+    isCalibrated = true;
+  }
+
+  flashLed();
 
   // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
@@ -149,8 +159,8 @@ void loop() {
     // check for overflow (this should never happen unless our code is too inefficient)
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
-        mpu.resetFIFO();
         Log.error("FIFO overflow!\n");
+        mpu.resetFIFO();
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
@@ -197,7 +207,13 @@ void loop() {
         }
     }
 
-    delay(1);
+    delay(15);
+}
+
+void flashLed() {
+  // Loop led indicator change mode
+  intLed = (intLed == LOW || intLed == 0) ? HIGH : LOW;
+  digitalWrite(LED_INDICATOR, intLed);
 }
 
 /// Check if the hand position is in potenital direction to the mouth.
@@ -215,5 +231,131 @@ bool checkPotentialPosition(float yaw, float pitch, float roll) {
 
 bool checkPotentialAccel(float x, float y, float z) {
     return true;
+}
+
+void calibrate() {
+  while(state < 3) {
+    if (state==0){
+      Log.trace("Reading sensors for first time...\n");
+      meansensors();
+      state++;
+      delay(1000);
+    }
+  
+    if (state==1) {
+      Log.trace("Calculating offsets...\n");
+      calibration();
+      state++;
+      delay(1000);
+    }
+
+    #ifdef DEBUG
+    if (state==2) {
+      meansensors();
+      Log.trace("FINISHED Calibration:\t");
+      
+      Serial.print(mean_ax); 
+      Serial.print("\t");
+      Serial.print(mean_ay); 
+      Serial.print("\t");
+      Serial.print(mean_az); 
+      Serial.print("\t");
+      Serial.print(mean_gx); 
+      Serial.print("\t");
+      Serial.print(mean_gy); 
+      Serial.print("\t");
+      Serial.println(mean_gz);
+      Serial.print("Your offsets:\t");
+      Serial.print(ax_offset); 
+      Serial.print("\t");
+      Serial.print(ay_offset); 
+      Serial.print("\t");
+      Serial.print(az_offset); 
+      Serial.print("\t");
+      Serial.print(gx_offset); 
+      Serial.print("\t");
+      Serial.print(gy_offset); 
+      Serial.print("\t");
+      Serial.println(gz_offset); 
+      
+      Log.trace("\nData is printed as: acelX acelY acelZ giroX giroY giroZ");
+      state++;
+    }
+    #endif
+  }
+}
+
+void meansensors(){
+  long i=0,buff_ax=0,buff_ay=0,buff_az=0,buff_gx=0,buff_gy=0,buff_gz=0;
+
+  while (i<(buffersize+101)){
+    // read raw accel/gyro measurements from device
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    
+    if (i>100 && i<=(buffersize+100)){ //First 100 measures are discarded
+      buff_ax=buff_ax+ax;
+      buff_ay=buff_ay+ay;
+      buff_az=buff_az+az;
+      buff_gx=buff_gx+gx;
+      buff_gy=buff_gy+gy;
+      buff_gz=buff_gz+gz;
+    }
+    if (i==(buffersize+100)){
+      mean_ax=buff_ax/buffersize;
+      mean_ay=buff_ay/buffersize;
+      mean_az=buff_az/buffersize;
+      mean_gx=buff_gx/buffersize;
+      mean_gy=buff_gy/buffersize;
+      mean_gz=buff_gz/buffersize;
+    }
+    i++;
+    delay(2); //Needed so we don't get repeated measures
+  }
+}
+
+void calibration(){
+  ax_offset=-mean_ax/8;
+  ay_offset=-mean_ay/8;
+  az_offset=(16384-mean_az)/8;
+
+  gx_offset=-mean_gx/4;
+  gy_offset=-mean_gy/4;
+  gz_offset=-mean_gz/4;
+  while (1){
+    int ready=0;
+    
+    flashLed();
+    
+    mpu.setXAccelOffset(ax_offset);
+    mpu.setYAccelOffset(ay_offset);
+    mpu.setZAccelOffset(az_offset);
+
+    mpu.setXGyroOffset(gx_offset);
+    mpu.setYGyroOffset(gy_offset);
+    mpu.setZGyroOffset(gz_offset);
+
+    meansensors();
+    Log.trace("In calibration...\n");
+
+    if (abs(mean_ax)<=acel_deadzone) ready++;
+    else ax_offset=ax_offset-mean_ax/acel_deadzone;
+
+    if (abs(mean_ay)<=acel_deadzone) ready++;
+    else ay_offset=ay_offset-mean_ay/acel_deadzone;
+
+    if (abs(16384-mean_az)<=acel_deadzone) ready++;
+    else az_offset=az_offset+(16384-mean_az)/acel_deadzone;
+
+    if (abs(mean_gx)<=giro_deadzone) ready++;
+    else gx_offset=gx_offset-mean_gx/(giro_deadzone+1);
+
+    if (abs(mean_gy)<=giro_deadzone) ready++;
+    else gy_offset=gy_offset-mean_gy/(giro_deadzone+1);
+
+    if (abs(mean_gz)<=giro_deadzone) ready++;
+    else gz_offset=gz_offset-mean_gz/(giro_deadzone+1);
+
+    if (ready==6) break;
+  }
 }
 
